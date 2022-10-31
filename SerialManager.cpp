a2882e62c309d36f2cf0a8e24ac8a35f35c5fe1b
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2022-10-31 17:51:54 +0000

    Revert "Changed serial for 8MHz mini -"
    This reverts commit 74159249bb6d770f39a1780fa7e67ddfadaa3966.

--- SerialManager.cpp
+++ SerialManager.cpp
@@ -33,8 +33,7 @@ SerialManager::SerialManager(Stream * myserial) {
 
 void SerialManager::init() {
   while (!Serial && millis() < 5000); // wait max 5s for Serial to start
-  //Serial.begin(115200);
-  Serial.begin(57600);
+  Serial.begin(115200);
   new SerialManager(&Serial);
 #ifdef SERIAL3_COMMANDS
   Serial3.begin(115200);
